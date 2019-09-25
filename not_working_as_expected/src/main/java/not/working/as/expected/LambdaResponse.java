2127848c3c5f469f53e8e32e7b4b32f9bc032820
Thomas <thomas@loyalbe.io>
2019-09-25 11:02:33 +0100

    Fixup for response returns and packaging

---
+++ not_working_as_expected/src/main/java/not/working/as/expected/LambdaResponse.java
@@ -0,0 +1,12 @@
+package not.working.as.expected;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Response
+ */
+public class LambdaResponse {
+
+    private @Getter @Setter int status;
+}
